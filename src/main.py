6d0a00c6edcf853456112be473c576263b0ebc6e
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-11-16 21:16:48 +0100

    add the knight rider

--- src/main.py
+++ src/main.py
@@ -23,8 +23,21 @@ if __name__ == '__main__':
 	fixtures.append(fixture)
     
     while True:
-	for fixture in fixtures:
-            fixture.setChannels({'red':random.randint(0, 255),
-                                 'green':random.randint(0, 255),
-                                 'blue':random.randint(0, 255)})
-        time.sleep(1)
+        for i in range(60):
+            for fixture in fixtures:
+                fixture.setChannels({'red':random.randint(0, 255),
+                                     'green':random.randint(0, 255),
+                                     'blue':random.randint(0, 255)})
+            time.sleep(1)
+
+        for n in range(15):
+            for i in range(len(fixtures)):
+                fixtures[i - 1].setChannels({'red':0,'green':0,'blue':0})
+                fixtures[i].setChannels({'red':255,'blue':255,'green':255})
+                time.sleep(.01)
+
+            for i in range(len(fixtures) - 2, 0, -1):
+                fixtures[i + 1].setChannels({'red':0,'green':0,'blue':0})
+                fixtures[i].setChannels({'red':255,'blue':255,'green':255})
+                time.sleep(.01)
+
