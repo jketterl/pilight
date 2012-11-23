23a2db08aef550ff20eb19a8a938e4e4bd7dbbd4
Jakob Ketterl <jketterl@chipxonio.de>
2012-11-23 11:53:20 +0100

    fft-based beat-detection, phase 1

--- src/audio/BeatDetector.py
+++ src/audio/BeatDetector.py
@@ -7,6 +7,23 @@ Created on Nov 21, 2012
 from audio.AudioReader import AudioReader
 import threading, numpy, struct
 
+class Band(object):
+    def __init__(self, number):
+        self.number = number
+        self.backlog = []
+        self.beat = False
+    def update(self, data):
+        powers = numpy.average(numpy.power(data, 2))
+        self.backlog.append(powers)
+        while len(self.backlog) > 50: self.backlog.pop(0)
+        if powers > numpy.average(self.backlog) * 1.3 and not self.beat:
+            #print '%d: BEAT' % self.number
+            self.beat = True
+        elif self.beat:
+            self.beat = False
+    def hasBeat(self):
+        return self.beat
+
 class BeatDetector(threading.Thread):
     def __init__(self, delegate):
         self.delegate = delegate
@@ -17,8 +34,11 @@ class BeatDetector(threading.Thread):
         audioreader = AudioReader.instance()
         #audioreader.start()
         
-        backlog = []
         beat = False
+        
+        bands = []
+        for i in range(64):
+            bands.append(Band(i))
         
         while self.doRun:
             audioreader.event.wait()
@@ -27,18 +47,27 @@ class BeatDetector(threading.Thread):
             form = '<%dh' % (audioreader.l * 2)
             data = struct.unpack(form, audioreader.data)
             
-            powers = numpy.power(data, 2)
-            powers = numpy.average(powers)
-            if powers < 0: continue
+            #start = time.time()
+            fft = numpy.abs(numpy.fft.fft(data))
+            fft = fft[0:len(fft)/2]
+            ratio = float(len(fft)) / len(bands)
+            for index, band in enumerate(bands):
+                band.update(fft[int(index * ratio):int((index + 1) * ratio)])
             
-            backlog.append(powers)
-            while len(backlog) > 50: backlog.pop(0)
-
-            if powers > numpy.average(backlog) * 1.3 and not beat:
+            #elapsed = time.time() - start
+            #print 'fft time is: ' + str(elapsed)
+            
+            count = 0
+            for band in bands:
+                if band.hasBeat(): count += 1
+                
+            if count > 32 and not beat:
                 self.delegate.onBeat()
                 beat = True
-            else:
+            elif beat:
                 beat = False
+            
+            
         #audioreader.stop()
     def stop(self):
         self.doRun = False
