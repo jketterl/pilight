597761cd6163971e2149a85dab69c8ce999f1050
Jakob Ketterl <jakob.ketterl@gmx.de>
2012-12-03 21:54:27 +0100

    complete implementation of artnet input

--- src/input/ArtnetInput.py
+++ src/input/ArtnetInput.py
@@ -2,16 +2,19 @@ from . import Input
 import threading, socket
 from artnet import packet
 
-print Input
-
 class ArtnetInput(Input):
-    def __init__(self):
-        sock = ArtnetSocket()
+    def __init__(self, universe):
+        self.universe = universe
+        sock = ArtnetSocket(self)
         sock.start()
+    def receive(self, p):
+        for i in range(min(512, len(self.universe))):
+            self.universe[i].setValue(p[i])
 
 class ArtnetSocket(threading.Thread):
-    def __init__(self):
+    def __init__(self, input):
         self.doRun = True
+        self.input = input
         super(ArtnetSocket, self).__init__()
     def run(self):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,4 +24,5 @@ class ArtnetSocket(threading.Thread):
         while (self.doRun):
             data, addr = sock.recvfrom(1024)
             p = packet.ArtNetPacket.parse(addr, data)
-            print p
+            if isinstance(p, packet.DmxPacket):
+                self.input.receive(p)
--- src/pwmslave.py
+++ src/pwmslave.py
@@ -5,9 +5,14 @@ sys.path.append('../vendors/adafruit/Adafruit_PWM_Servo_Driver/')
 from output import Output
 from filter import ScalingAlphaFilter
 from input import ArtnetInput
+from universe import Universe
 
 if __name__ == '__main__':
+
     output = Output.factory('AdafruitPWMOutput', address=0x40, bus=1)
     output.addFilter(ScalingAlphaFilter(4095))
 
-    input = ArtnetInput()
+    universe = Universe(16)
+    universe.setOutput(output)
+
+    input = ArtnetInput(universe)
