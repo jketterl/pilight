6211218db236f4e45f9a085ed1943d430e5f60e8
Jakob Ketterl <jketterl@chipxonio.de>
2012-12-16 18:37:21 +0100

    allow direct mapping against universes (using an offset)

--- src/fixture.py
+++ src/fixture.py
@@ -14,11 +14,17 @@ class Fixture(object):
     def setChannels(self, values):
         for key in values:
             self.mappings[key].setValue(values[key]);
+    def mapToUniverse(self, universe, offset = 0):
+        channelMap = {}
+        for channel in self.getInputs():
+            channelMap[channel] = universe[offset]
+            offset += 1
+        self.mapToChannels(channelMap)
     
 class RGBFixture(Fixture):
     def getInputs(self):
         return [
-            'red',
             'green',
+            'red',
             'blue'
         ]
