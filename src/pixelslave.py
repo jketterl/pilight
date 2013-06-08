eac0ee110c73ba6551db618eda4e94accd544f3d
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-06-08 23:34:06 +0000

    add support for WS2801 chip driven led strips

---
+++ src/pixelslave.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('../vendors/python-artnet/src/')
+
+from output import Output
+from filter import ScalingAlphaFilter
+from input import ArtnetInput
+from universe import Universe
+
+if __name__ == '__main__':
+
+    output = Output.factory('WS2801Output', channels=150)
+    output.addFilter(ScalingAlphaFilter(255))
+
+    universe = Universe(150)
+    universe.setOutput(output)
+
+    input = ArtnetInput(universe)
