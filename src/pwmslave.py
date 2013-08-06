29f059eeec6e1e64fe0ad9e9a1b6fc66815c642e
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-08-06 19:16:29 +0200

    socket input for pwmslave

--- src/pwmslave.py
+++ src/pwmslave.py
@@ -1,10 +1,9 @@
 import sys
-sys.path.append('../vendors/python-artnet/src/')
 sys.path.append('../vendors/adafruit/Adafruit_PWM_Servo_Driver/')
 
 from output import Output
 from filter import ScalingAlphaFilter
-from input import ArtnetInput
+from input import SocketInput
 from universe import Universe
 
 if __name__ == '__main__':
@@ -15,4 +14,4 @@ if __name__ == '__main__':
     universe = Universe(16)
     universe.setOutput(output)
 
-    input = ArtnetInput(universe)
+    input = SocketInput(universe, '192.168.4.2', 'pwm')
