733e083705253cd294ff7209ced3513caac8b928
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-08-06 19:36:17 +0200

    reconnect client socket as soon as possible

--- src/input/SocketInput.py
+++ src/input/SocketInput.py
@@ -1,17 +1,28 @@
 from . import Input
-import socket, struct
+import socket, struct, time
 
 class SocketInput(Input):
-	def __init__(self, universe, server, outputId):
-		self.universe = universe
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.connect((server, 9191))
-		self.socket.send('REQUEST %s' % outputId)
-		data = ''
-		while 1:
-			data += self.socket.recv(4096)
-			while len(data) >= 3:
-				(channel, value) = struct.unpack('HB', data[:3])
-				self.universe[channel].setValue(value)
-				data = data[3:]
-		super(SocketInput, self).__init__()
+    def __init__(self, universe, server, outputId):
+        super(SocketInput, self).__init__()
+        self.universe = universe
+        self.server = server
+        self.outputId = outputId
+        while True:
+            self.run()
+            time.sleep(10)
+    def run(self):
+        try:
+            self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket.connect((self.server, 9191))
+            self.socket.send('REQUEST %s' % self.outputId)
+            data = ''
+            while True:
+                data += self.socket.recv(4096)
+                if len(data) == 0:
+                    return
+                while len(data) >= 3:
+                    (channel, value) = struct.unpack('HB', data[:3])
+                    self.universe[channel].setValue(value)
+                    data = data[3:]
+        except socket.error:
+            pass
