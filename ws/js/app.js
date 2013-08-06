929b5d94356dd2896b535a525fe929f88b8f8acd
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-08-06 14:55:41 +0200

    add state for the frontend

--- ws/js/app.js
+++ ws/js/app.js
@@ -7,6 +7,8 @@ Ext.onReady(function(){
 
     var viewport = Ext.create('Ext.Viewport')
 
+    Ext.state.Manager.setProvider(new Ext.state.CookieProvider());
+
 	var socket = Ext.create('pilight.socket.Socket', {
 		host:window.location.host,
 		port:9001
@@ -21,10 +23,13 @@ Ext.onReady(function(){
         socket.sendCommand({'command':'getControllables'}, function(data){
             data.forEach(function(controllable){
                 var type = controllable.type;
+                var id = controllable.id;
                 if (typeof(typemap[type]) == 'undefined') return;
                 var win = Ext.create('Ext.window.Window', {
                     layout:'fit',
                     title:type,
+                    stateful:true,
+                    stateId:id,
                     items:[
                         Ext.create(typemap[type], {
                             socket:socket
