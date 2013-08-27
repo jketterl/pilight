422d47d54c277f4fd0ad00d07db882354ab7c9d7
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-08-27 22:04:52 +0200

    bands as instance variable

--- src/show/FFT.py
+++ src/show/FFT.py
@@ -4,14 +4,16 @@ from universe import Universe
 from .VUMeter import VUOutput
 
 class FFT(Show):
+    def __init__(self, *args, **kwargs):
+        self.bands = 12
+        super(FFT, self).__init__(*args, **kwargs)
     def run(self):
-        bands = 12
-        self.universe = Universe(bands)
+        self.universe = Universe(self.bands)
         for channel in self.universe:
             channel.addListener(self)
 
         self.outputs = []
-        self.ratio = float(len(self.fixtures)) / bands
+        self.ratio = float(len(self.fixtures)) / self.bands
         start = 0
         colorConfig = {
             'blue':{
@@ -23,12 +25,12 @@ class FFT(Show):
                 'end':1
             }
         }
-        for i in range(bands):
+        for i in range(self.bands):
             end = int((i+1) * self.ratio)
             self.outputs.append(VUOutput(self.fixtures[start:end], colorConfig))
             start = end
 
-        self.reader = FFTReader(AudioReader.instance("hw:1,0"), self.universe, bands)
+        self.reader = FFTReader(AudioReader.instance("hw:1,0"), self.universe, self.bands)
         self.reader.start()
         self.endEvent.wait()
     def onValueChange(self, channel, value):
@@ -42,3 +44,4 @@ class FFT(Show):
             output.stop()
         self.endEvent.set()
         super(FFT, self).stop()
+
