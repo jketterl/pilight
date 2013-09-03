b0e7f476e3017ba72576177fb9701d0b9b5b31f9
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-09-04 01:42:45 +0200

    fix direct fft shutdown

--- src/show/DirectFFT.py
+++ src/show/DirectFFT.py
@@ -4,10 +4,14 @@ class DirectFFT(FFT):
     def __init__(self, fixtures, bands, *args, **kwargs):
         super(DirectFFT, self).__init__(bands, *args, **kwargs)
         self.bands = 16
-        #self.fixtures = bands
     def onValueChange(self, channel, value):
         index = self.universe.channelIndex(channel)
-        #channels = ['red', 'green', 'blue']
-        #self.fixtures[int(index / 3)].setChannels({channels[index % 3]:value})
-        #print index
         self.fixtures[index].setChannels({'brightness':value})
+    def stop(self):
+        self.reader.stop()
+        self.reader.endEvent.wait()
+        for f in self.fixtures:
+            f.setChannels({'brightness':0});
+        self.endEvent.set()
+        super(FFT, self).stop()
+
