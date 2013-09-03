4d32692c37fab89791c6162d41a5c22858fb63f3
Jakob Ketterl <jakob.ketterl@gmx.de>
2013-09-04 01:35:16 +0200

    allow multiple outputs to be written over a single serial line

--- src/output/SerialOutput.py
+++ src/output/SerialOutput.py
@@ -1,10 +1,22 @@
 from .ThreadedOutput import ThreadedOutput
 import serial, struct
 
+class Writer(object):
+    _writer = None
+    @staticmethod
+    def getWriter():
+        if Writer._writer is None:
+            Writer._writer = Writer()
+        return Writer._writer
+    def __init__(self):
+        self.ser = serial.Serial('/dev/ttyAMA0', 115200)
+    def write(self, data):
+        self.ser.write(data)
+        self.ser.flushInput()
+
 class SerialOutput(ThreadedOutput):
     def __init__(self, deviceId, *args, **kwargs):
         super(SerialOutput, self).__init__(*args, **kwargs)
-        self.ser = serial.Serial('/dev/ttyAMA0', 115200)
         self.deviceId = deviceId
 
     def applyChanges(self, changes):
@@ -12,5 +24,4 @@ class SerialOutput(ThreadedOutput):
         for key in changes:
             message += struct.pack('BB', key, changes[key])
         message = 'SYNC' + struct.pack('<BH', self.deviceId, len(message)) + message
-        self.ser.write(message)
-        self.ser.flushInput()
+        Writer.getWriter().write(message)
