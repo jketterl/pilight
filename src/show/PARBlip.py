ee3057c05082b05109d238bcbfa8c12815a7166c
Jakob Ketterl <jakob.ketterl@gmx.de>
2015-01-14 20:50:03 +0100

    add a new bpm-controlled show to show off the led pars

---
+++ src/show/PARBlip.py
@@ -0,0 +1,40 @@
+from . import Show
+from audio import BeatDetector, BeatDelegate
+from fixture import FixtureManager
+from threading import Event
+import random
+
+class PARBlip(Show, BeatDelegate):
+    def __init__(self):
+        super(PARBlip, self).__init__()
+        self.event = Event()
+    def run(self):
+        fixtures = FixtureManager.filter(lambda f : f.hasTag('par'))
+
+        detector = BeatDetector(self)
+        detector.start();
+
+        while (self.doRun):
+            self.event.wait()
+            for fixture in fixtures:
+                fixture.setChannels(self.generateColor())
+            self.event.clear()
+
+        for fixture in fixtures:
+            fixture.setChannels({'red':0, 'green':0, 'blue':0})
+
+        detector.stop()
+        self.endEvent.set()
+    def generateColor(self):
+        rs = random.randint(1, 7)
+        value = {}
+
+        for color in ['red', 'green', 'blue']:
+            value[color] = (rs & 1) * 255
+            rs = rs >> 1
+        return value
+    def onBeat(self):
+        self.event.set()
+    def stop(self):
+        self.event.set()
+        super(PARBlip, self).stop()
