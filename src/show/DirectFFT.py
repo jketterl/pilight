cd5f09f04fb95786bfdac7c9d5f9131a5810a3b5
Jakob Ketterl <jakob.ketterl@gmx.de>
2015-01-19 21:05:55 +0100

    put fft on pars

--- src/show/DirectFFT.py
+++ src/show/DirectFFT.py
@@ -4,13 +4,13 @@ from fixture import FixtureManager
 class DirectFFT(FFT):
     def __init__(self, bands, *args, **kwargs):
         super(DirectFFT, self).__init__(*args, **kwargs)
-        self.bands = 6
-        self.colorConfig = {
-            'brightness' : { 'start' : 0, 'end' : 1}
-        }
-        self.fixtures = self.getFixtures()
+
+        # bands contains number of bands in parent
+        self.bands = len(bands)
+        # so we have to store this somewhere else
+        self.b = bands
     def getFixtures(self):
         return FixtureManager.filter(lambda f : f.hasTag('dmx'))
     def onValueChange(self, channel, value):
         index = self.universe.channelIndex(channel)
-        self.fixtures[index].setChannels({'brightness':value})
+        self.b[index].setValue(value)
