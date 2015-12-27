bd8247f52584269abfe77d6a41ce03053021e681
Jakob Ketterl <jakob.ketterl@gmx.de>
2015-12-27 21:17:51 +0100

    use gpio interrupts to avoid polling loops

--- src/gpio/__init__.py
+++ src/gpio/__init__.py
@@ -1,23 +1,22 @@
 import RPi.GPIO as GPIO
 import time
-from threading import Thread
 
 from universe import Universe
 
-class GPIOInput(Thread):
+class GPIOInput(object):
     def __init__(self, gpios, invert = True):
         GPIO.setmode(GPIO.BCM)
         self.gpios = gpios
         self.universe = Universe(len(gpios))
         self.invert = invert
-        for gpio in gpios:
-            GPIO.setup(gpio, GPIO.IN)
+        GPIO.setup(gpios, GPIO.IN)
 
         super(GPIOInput, self).__init__()
 
-        self.start()
-    def run(self):
-        while (True):
-            for index, gpio in enumerate(self.gpios):
-                self.universe[index].setValue(255 if GPIO.input(gpio) != self.invert else 0)
-            time.sleep(.1)
+        def gpioChange(index):
+            def f(channel):
+                self.universe[index].setValue(255 if GPIO.input(channel) != self.invert else 0)
+            return f
+
+        for index, gpio in enumerate(self.gpios):
+            GPIO.add_event_detect(gpio, GPIO.BOTH, callback = gpioChange(index))
