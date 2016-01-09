377595c601ae05dd790af9a98787e1ad56a50037
Jakob Ketterl <jakob.ketterl@gmx.de>
2016-01-09 16:28:17 +0100

    fix artnet broadcast

--- src/output/ArtnetOutput.py
+++ src/output/ArtnetOutput.py
@@ -6,6 +6,7 @@ class ArtnetOutput(BufferedOutput):
     def __init__(self, target):
         self.target = target
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.socket.connect((target, 6454))
         super(ArtnetOutput, self).__init__(256)
     def write(self):
