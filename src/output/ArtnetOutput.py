380f88cfe7399b8a523de4c3e42e4a653873f136
Jakob Ketterl <jakob.ketterl@gmx.de>
2016-03-07 23:12:26 +0100

    allow artnet universes to be set

--- src/output/ArtnetOutput.py
+++ src/output/ArtnetOutput.py
@@ -3,14 +3,15 @@ from artnet.packet import *
 import socket
 
 class ArtnetOutput(BufferedOutput):
-    def __init__(self, target):
+    def __init__(self, universe, target):
+        self.artnetUniverse = universe
         self.target = target
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, 0)
         self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.socket.connect((target, 6454))
         super(ArtnetOutput, self).__init__(256)
     def write(self):
-        packet = DmxPacket()
+        packet = DmxPacket(universe = self.artnetUniverse)
         for i, value in enumerate(self.buffer):
             packet[i] = value
         self.socket.sendall(packet.encode())
